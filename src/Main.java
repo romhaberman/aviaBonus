6b4379700dbd24fc06335a02bd885ee33e8abd1d
Roman Khabarov <habermanrom@gmail.com>
2024-04-27 21:26:57 +1000

    main bonus

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class mili {
+    public static void main(String[] args) {
+        int ticketcost = 13676;
+        int bonus = 20;
+
+        int bonusMile = ticketcost / bonus;
+
+        System.out.println(bonusMile);
+
+    }
+}
